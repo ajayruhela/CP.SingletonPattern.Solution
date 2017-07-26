c32be5f63813bb4c9786dc8ec38b10ad8617d660
ajayruhela <jrhl0013@gmail.com>
2017-07-26 14:01:20 +0200

    singleton patter

---
+++ CP.SingletonPattern.BLL/BusinesEntities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP.SingletonPattern.BLL
+{
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+   public class Singleton
+    {
+        private static Singleton _instance;
+        private static bool ifCreated = false;
+        // Constructor is 'protected'
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+                ifCreated = true;
+            }
+            else
+            {
+                ifCreated = false;
+            }
+            return _instance;
+        }
+
+        public bool TellMeIfCreated()
+        {
+            return ifCreated;
+        }
+    }
+}
---
+++ CP.SingletonPattern.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CP.SingletonPattern.BLL;
+namespace CP.SingletonPattern.ConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// MainApp startup class for Structural
+        /// Singleton Design Pattern.
+        /// </summary>
+ 
+            static void Main()
+            {
+                // Constructor is protected -- cannot use new
+                Singleton s1 = Singleton.Instance();
+            Console.WriteLine("if s1 Created: {0:s}", s1.TellMeIfCreated());
+            Singleton s2 = Singleton.Instance();
+            Console.WriteLine("if s2 Created: {0:s}", s2.TellMeIfCreated());
+
+            // Test for same instance
+            if (s1 == s2)
+                {
+                    Console.WriteLine("Objects are the same instance");
+                }
+            Console.WriteLine("HashCode s1 {0}",s1.GetHashCode());
+            Console.WriteLine("HashCode s2 {0}", s2.GetHashCode());
+                // Wait for user
+                Console.ReadKey();
+            }
+     }
+}
