89263505ad925dd1ad4a0ccd0754c4904bfa1e6c
ajayruhela <jrhl0013@gmail.com>
2017-07-26 15:00:45 +0200

    real example

--- CP.SingletonPattern.ConsoleApp/Program.cs
+++ CP.SingletonPattern.ConsoleApp/Program.cs
@@ -28,8 +28,28 @@ namespace CP.SingletonPattern.ConsoleApp
                 }
             Console.WriteLine("HashCode s1 {0}",s1.GetHashCode());
             Console.WriteLine("HashCode s2 {0}", s2.GetHashCode());
-                // Wait for user
-                Console.ReadKey();
+            // Wait for user
+
+            Console.WriteLine("=================LoadBalancer============");
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+            Console.ReadKey();
             }
      }
 }
